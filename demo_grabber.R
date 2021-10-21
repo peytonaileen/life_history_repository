b355982279bcd0614952ad4aad1940a9e43e8220
peytonaileen <pamooreb@ucsb.edu>
2021-10-20 20:01:02 -0700

    Added theme to document but now it will not work as a Shiny document.

--- demo_grabber.R
+++ demo_grabber.R
@@ -4,7 +4,26 @@ library(dplyr)
 
 
 
-fish <- new("LifeHistory", title = "trial", speciesName = "tuna", shortDescription = "here is a fish" )
+fish <- new("LifeHistory",
+            title = "trial",
+            speciesName = "Naso Unicornis",
+            shortDescription = "This species is commonly known as the orange spine unicornfish", 
+            longDescription = "This fish is an herbivore. It is highly targeted in tropical reef fisheries around the world", 
+            author = "Peyton Moore", 
+            authAffiliaton = "UCSB", 
+            reference = "Harford et. al 2021", 
+            L_units = "cm",
+            Walpha_units = "cm", 
+            Linf = 4, 
+            K = 0.3, 
+            L50 = 0.34, 
+            L95 = 0.6, 
+            M = 0.56, 
+            MK = 25, 
+            LW_A = 4.5,
+            LW_B = 0.53, 
+            Tmax = 76, 
+            Steep = 1.3)
 
 
 perch <- new("LifeHistory", title = "second trial", speciesName = "opal eye", shortDescription = "this is another fish - low trophic level!" )
